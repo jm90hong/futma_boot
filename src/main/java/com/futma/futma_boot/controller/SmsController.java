d7ddfcd6bfb539198e18126c7f9e29b5f7786add
jm90hong <jm90.hong@gmail.com>
2024-01-16 16:00:07 +0900

    1

--- src/main/java/com/futma/futma_boot/controller/SmsController.java
+++ src/main/java/com/futma/futma_boot/controller/SmsController.java
@@ -1,8 +1,11 @@
 package com.futma.futma_boot.controller;
 
+import java.util.Random;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.futma.futma_boot.service.SmsService;
@@ -15,14 +18,38 @@ public class SmsController {
 	SmsService smsService;
 	
 	
-	@GetMapping("send")
+	@GetMapping("sendCode")
 	public String send(
-				
+				@RequestParam(value="toTel") String toTel
 			) {
 		
+		Random random = new Random();		//랜덤 함수 선언
+		int createNum = 0;  			//1자리 난수
+		String ranNum = ""; 			//1자리 난수 형변환 변수
+        	int letter    = 6;			//난수 자릿수:6
+		String resultNum = "";  		//결과 난수
 		
-		smsService.send();
+		for (int i=0; i<letter; i++) { 
+            		
+			createNum = random.nextInt(9);		//0부터 9까지 올 수 있는 1자리 난수 생성
+			ranNum =  Integer.toString(createNum);  //1자리 난수를 String 으로 형변환
+			resultNum += ranNum;			//생성된 난수(문자열)을 원하는 수(letter)만큼 더하며 나열
+		}	
 		
-		return "ok";
+		
+		String message = "[Futma.] 인증코드는 ["+resultNum+"] 입니다.";
+		
+		boolean chk = smsService.sendSMS(toTel,message);
+		
+		if(chk) {
+			return resultNum;
+		}else {
+			return "fail";
+		}
+		
+		
 	}
+	
+	
+	
 }
--- src/main/java/com/futma/futma_boot/service/SmsService.java
+++ src/main/java/com/futma/futma_boot/service/SmsService.java
@@ -9,24 +9,29 @@ import net.nurigo.sdk.message.service.DefaultMessageService;
 
 @Service
 public class SmsService {
+	
+	static String ADMIN_TEL_NUMBER = "01099361422";
 
-	public void send() {
+	public boolean sendSMS(String toTelNumber, String m) {
 		DefaultMessageService messageService =  NurigoApp.INSTANCE.initialize("NCSDI4IFRRU5F6WR", "LJTVHE00KXFETQCMSCJXFC0SQVENLWBT", "https://api.coolsms.co.kr");
 		// Message 패키지가 중복될 경우 net.nurigo.sdk.message.model.Message로 치환하여 주세요
 		Message message = new Message();
-		message.setFrom("01099361422");
-		message.setTo("01082320062");
-		message.setText("Futma. 인증번호 [123456] 입니다.");
+		message.setFrom(ADMIN_TEL_NUMBER);
+		message.setTo(toTelNumber);
+		message.setText(m);
 
 		try {
 		  // send 메소드로 ArrayList<Message> 객체를 넣어도 동작합니다!
 		  messageService.send(message);
+		  return true;
 		} catch (NurigoMessageNotReceivedException exception) {
 		  // 발송에 실패한 메시지 목록을 확인할 수 있습니다!
-		  System.out.println(exception.getFailedMessageList());
-		  System.out.println(exception.getMessage());
+//		  System.out.println(exception.getFailedMessageList());
+//		  System.out.println(exception.getMessage());
+		  return false;
 		} catch (Exception exception) {
-		  System.out.println(exception.getMessage());
+//		  System.out.println(exception.getMessage());
+		  return false;
 		}
 	}
 	
