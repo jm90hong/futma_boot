cd6934a94421dc89fc49bc8b87faa13a5985bced
jm90hong <jm90.hong@gmail.com>
2024-07-30 12:49:13 +0900

    1

---
+++ src/main/java/com/futma/futma_boot/service/FirebaseService.java
@@ -0,0 +1,18 @@
+package com.futma.futma_boot.service;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class FirebaseService {
+
+	
+	
+	
+	@PostConstruct
+	public void initialize() {
+		System.out.println("초기화");
+	}
+}
