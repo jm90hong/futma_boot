af74cb7a0ad60cc42c0447f8f53979873f7c2c7a
jm90hong <jm90.hong@gmail.com>
2025-03-14 20:00:17 +0900

    0314

--- src/main/java/com/futma/futma_boot/controller/MainController.java
+++ src/main/java/com/futma/futma_boot/controller/MainController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MainController {
 	
 	
-	
+	//http://43.201.51.210:8080/futma_boot/
 	@GetMapping("/")
 	public String home() {
 		return "home";
